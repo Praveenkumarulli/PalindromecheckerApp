7777b7ecd9dfba34b9093992c6c4b61feb39c972
Praveenkumarulli <praveenulli2006@gmail.com>
2026-02-27 09:58:19 +0530

    UC6:Queue + Stack Based Palindrome Check

--- src/PalindromecheckerApp.java
+++ src/PalindromecheckerApp.java
@@ -1,17 +1,25 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class PalindromecheckerApp {
     public static void main(String[] args) {
         String word = "madam";
+        Queue<Character> queue = new LinkedList<>();
         Stack<Character> stack = new Stack<>();
 
         for (int i = 0; i < word.length(); i++) {
-            stack.push(word.charAt(i));
+            char c = word.charAt(i);
+            queue.add(c);
+            stack.push(c);
         }
 
-        String reversed = "";
-        while (!stack.isEmpty()) {
-            reversed = reversed + stack.pop();
+        boolean isPalindrome = true;
+        while (!queue.isEmpty() && !stack.isEmpty()) {
+            if (queue.remove() != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
         }
 
         System.out.println("=======================================");
@@ -20,7 +28,7 @@ public class PalindromecheckerApp {
         System.out.println("=======================================");
         System.out.println("Checking word: " + word);
 
-        if (word.equals(reversed)) {
+        if (isPalindrome) {
             System.out.println("Result: \"" + word + "\" is a palindrome.");
         } else {
             System.out.println("Result: \"" + word + "\" is not a palindrome.");
